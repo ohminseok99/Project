7b0b50f04ab410f6b30afb6368d6bf37d055555d
wooyong99 <wulovesk@naver.com>
2023-04-03 23:09:05 +0900

    model.py 작성

--- order/models.py
+++ order/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='사용자')
+    # 유의할 것은 Foreign키의 필수 매개변수가 삭제될 경우 참조한 객체는 어떻게 할지를 결정하는 옵션이 on_delete입니다.
+    # 첫번째 인자는 settings앱에 등록된 user앱의 models.py의 User클래스를 사용하겠다는 의미
+
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
+    quantity = models.IntegerField(verbose_name='수량')
+    createDate = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    # 관리자 페이지에서 객체를 확인할 때 속성값으로 받게되면 해당 값의 명이 출력된다.
+    def __str__(self):
+        return self.name
+
+    class Meta: # 관리자 페이지 설정
+        db_table = 'my_order' # DB에 저장될 테이블 이름 지정
+        verbose_name='주문'
+        verbose_name_plural = '주문'
+        # 복수형 지정안하면 주문s로 된다.
--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(verbose_name='이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    createDate = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+    # 처음 객체가 생성된 날짜를 자동으로 지정하기 위해 auto_now_add로 지정
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'my_user'
+        verbose_name='고객'
+        verbose_name_plural='고객'

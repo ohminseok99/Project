a3328fa894e4f15c4c2589408473df05b25584a9
wooyong99 <wulovesk@naver.com>
2023-04-04 00:42:50 +0900

    회원가입 로직작성

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hi),
+    path('',views.index),
+    path('join/', views.join),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.http import HttpResponseRedirect
+from user.forms import RegisterForm
 
-# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def join(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponseRedirect('/admin')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form' : form })

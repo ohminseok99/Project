1fa807b1eb58fa3f16d7f3b190e7d21e24a58242
wooyong99 <wulovesk@naver.com>
2023-04-11 18:42:30 +0900

    LoginForm 작성

--- user/forms.py
+++ user/forms.py
@@ -40,3 +40,34 @@ class RegisterForm(forms.Form):
                     password=password,
                 )
                 user.save()  # db저장
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64,
+        label='이메일',
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력하세요.'
+        },
+        widget=forms.PasswordInput,
+        label='비밀번호'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if password and email :
+            try:
+                user = User.objects.get(email = email)
+            except User.DoesNotExist:
+                self.add_error('email', '해당 이메일이 없습니다.')
+                return
+            if not check_password(password, user.password):
+                self.add_error('password','비밀번호가 틀렸습니다.')
+            else:
+                self.email = user.email
